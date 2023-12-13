12ba5b0e71ff90f98ab4752e0d8adbb1f3ad6f29
Martin Petrov <martinmpetrov@hotmail.com>
2023-12-14 00:48:38 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { z } from "zod";
+import { useForm } from "@prismane/core/hooks";
+import {
+  Center,
+  Card,
+  Form,
+  Button,
+  TextField,
+  PasswordField,
+} from "@prismane/core";
+
+import p from "./zodToPrismane";
 
 function App() {
+  // const { handleSubmit, handleReset, register } = useForm({
+  //   fields: {
+  //     username: {
+  //       value: "",
+  //       validators: {
+  //         required: (v) => p(v, z.string()),
+  //         min: (v) => p(v, z.string().min(5)),
+  //       },
+  //     },
+  //     password: {
+  //       value: "",
+  //       validators: {
+  //         required: (v) => p(v, z.string()),
+  //         min: (v) => p(v, z.string().min(5)),
+  //       },
+  //     },
+  //   },
+  // });
+
+  const { handleSubmit, handleReset, register } = useForm({
+    fields: {
+      username: {
+        value: "",
+        validators: {
+          required: (v) =>
+            p(
+              v,
+              z.string({
+                required_error: "The username field is required!",
+              })
+            ),
+          min: (v) =>
+            p(
+              v,
+              z.string().min(5, {
+                message: "Username must be longer than 5 characters!",
+              })
+            ),
+        },
+      },
+      password: {
+        value: "",
+        validators: {
+          required: (v) =>
+            p(
+              v,
+              z.string({
+                required_error: "The password field is required!",
+              })
+            ),
+          min: (v) =>
+            p(
+              v,
+              z.string().min(5, {
+                message: "Password must be longer than 5 characters!",
+              })
+            ),
+        },
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Center w="100vw" h="100vh">
+      <Card w={300}>
+        <Form
+          onSubmit={(e: any) => {
+            handleSubmit(e, (v: any) => console.log(v));
+          }}
+          autoComplete="off"
+          onReset={() => handleReset()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <TextField
+            placeholder="Enter username: "
+            label="Username:"
+            {...register("username")}
+          />
+          <PasswordField
+            placeholder="Enter password: "
+            label="Password:"
+            {...register("password")}
+          />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card>
+    </Center>
   );
 }
 
